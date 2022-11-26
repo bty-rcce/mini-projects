32c5e2452402a12f7a033e76c0ab95250af0043b
bty_rcce <betterthanyesterday.rce@gmail.com>
2022-11-26 18:50:48 +0800

    updated acronym/index.js with correct function code

--- acronym/index.js
+++ acronym/index.js
@@ -1,5 +1,19 @@
-function acronym(string){
+function acronym(sentence){
     // get the first letter of each string
+    var acronym = "";
+
+    // split words from sentence
+    var words = sentence.split(" ");
+    console.log(words);
     
-    
+    // go through each word in words
+    for (i in words) {
+        // get the first letter of each string
+        letters = words[i].split("");
+        acronym += letters[0];
+    }
+    return acronym;
 }
+
+// var sentence = prompt("Enter a short sentence ");
+console.log(acronym("Rovin Czar"));
